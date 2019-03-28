025a238cbbf7faded3c3b4cb8d38ae32cc6f76c5
Aurish Hammad Hafeez <ahafeez@knysys.com>
2019-03-28 11:44:21 +0500

    script test

--- setup.py
+++ setup.py
@@ -7,4 +7,6 @@ setup(
     version      = '1.0',
     packages     = find_packages(),
     entry_points = {'scrapy': ['settings = books_crawler.settings']},
+    scripts      = ['bin/hello.py'],
+
 )
